b3bd86b8ef07052efbdf22918c5353bc43b26996
KelwayHao <kelwaycf@gmail.com>
2022-05-20 10:35:00 +0300

    Add RxJava

--- app/src/main/java/com/example/retrofithomework/presentation/SplashScreen.kt
+++ app/src/main/java/com/example/retrofithomework/presentation/SplashScreen.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 import com.example.retrofithomework.R
 import com.example.retrofithomework.domain.models.NewsInfo
 import com.example.retrofithomework.presentation.viewmodel.NewsViewModel
@@ -26,12 +27,23 @@ class SplashScreen : AppCompatActivity() {
         viewModel.setWordSplashScreen("Android",3L, 4L)
     }
 
+    private lateinit var observer: Observer<NewsInfo>
+
     private fun initObserver() {
-        viewModel.news.observe(this, ) { listNews ->
+
+        observer = Observer<NewsInfo> { listNews->
             intent = Intent(this, MainActivity::class.java)
             intent.putExtra("Android", listNews)
             startActivity(intent)
             finish()
+
         }
+
+        viewModel.news.observe(this, observer)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        viewModel.news.removeObserver(observer)
     }
 }
